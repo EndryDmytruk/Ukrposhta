1b601ecd80cace5577187e0b05985ad45c88c54e
Endry Dmytruk <endry01234567@gmail.com>
2022-05-02 19:42:02 +0300

    Added functionality for report

--- Project/Task_042022/MenuForm/Home.Designer.cs
+++ Project/Task_042022/MenuForm/Home.Designer.cs
@@ -28,28 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.MenuStrip();
             this.Departments_Menu = new System.Windows.Forms.ToolStripMenuItem();
             this.Positions_Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.Premiums_Menu = new System.Windows.Forms.ToolStripMenuItem();
             this.Employees_Menu = new System.Windows.Forms.ToolStripMenuItem();
             this.Payments_Menu = new System.Windows.Forms.ToolStripMenuItem();
             this.Report_Menu = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ListEmployees_Table = new System.Windows.Forms.DataGridView();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.Menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ListEmployees_Table)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // Menu
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.Departments_Menu,
             this.Positions_Menu,
+            this.Premiums_Menu,
             this.Employees_Menu,
             this.Payments_Menu,
             this.Report_Menu});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(800, 24);
+            this.Menu.TabIndex = 0;
+            this.Menu.Text = "menuStrip1";
             // 
             // Departments_Menu
             // 
@@ -63,6 +69,12 @@
             this.Positions_Menu.Name = "Positions_Menu";
             this.Positions_Menu.Size = new System.Drawing.Size(122, 20);
             this.Positions_Menu.Text = "Редагування посад";
+            // 
+            // Premiums_Menu
+            // 
+            this.Premiums_Menu.Name = "Premiums_Menu";
+            this.Premiums_Menu.Size = new System.Drawing.Size(129, 20);
+            this.Premiums_Menu.Text = "Редагування премій";
             // 
             // Employees_Menu
             // 
@@ -81,21 +93,58 @@
             this.Report_Menu.Name = "Report_Menu";
             this.Report_Menu.Size = new System.Drawing.Size(40, 20);
             this.Report_Menu.Text = "Звіт";
+            this.Report_Menu.Click += new System.EventHandler(this.Report_Menu_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(776, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "ФІльтр по відділах";
+            // 
+            // ListEmployees_Table
+            // 
+            this.ListEmployees_Table.AllowUserToAddRows = false;
+            this.ListEmployees_Table.AllowUserToDeleteRows = false;
+            this.ListEmployees_Table.AllowUserToResizeColumns = false;
+            this.ListEmployees_Table.AllowUserToResizeRows = false;
+            this.ListEmployees_Table.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.ListEmployees_Table.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListEmployees_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListEmployees_Table.Location = new System.Drawing.Point(12, 56);
+            this.ListEmployees_Table.MultiSelect = false;
+            this.ListEmployees_Table.Name = "ListEmployees_Table";
+            this.ListEmployees_Table.ReadOnly = true;
+            this.ListEmployees_Table.RowHeadersVisible = false;
+            this.ListEmployees_Table.RowTemplate.Height = 25;
+            this.ListEmployees_Table.ShowEditingIcon = false;
+            this.ListEmployees_Table.Size = new System.Drawing.Size(776, 382);
+            this.ListEmployees_Table.TabIndex = 2;
+            this.ListEmployees_Table.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListEmployees_Table_CellClick);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Filter = "Text files(*.txt)|*.txt";
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.ListEmployees_Table);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Menu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.MainMenuStrip = this.menuStrip1;
+            this.MainMenuStrip = this.Menu;
             this.MaximizeBox = false;
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ListEmployees_Table)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,11 +152,15 @@
 
         #endregion
 
-        private MenuStrip menuStrip1;
+        private MenuStrip Menu;
         private ToolStripMenuItem Departments_Menu;
         private ToolStripMenuItem Positions_Menu;
         private ToolStripMenuItem Employees_Menu;
         private ToolStripMenuItem Payments_Menu;
         private ToolStripMenuItem Report_Menu;
+        private ComboBox comboBox1;
+        private DataGridView ListEmployees_Table;
+        private ToolStripMenuItem Premiums_Menu;
+        private SaveFileDialog saveFile;
     }
 }
--- Project/Task_042022/MenuForm/Home.cs
+++ Project/Task_042022/MenuForm/Home.cs
@@ -1,16 +1,42 @@
+using Task_042022.Controllers;
+
 namespace Task_042022.MenuForm
 {
     public partial class Home : Form
     {
+        private readonly HomeController controller;
+
         public Home()
         {
             InitializeComponent();
+
+            UpdateTable();
+            controller = new HomeController();
         }
 
         private void Departments_Menu_Click(object sender, EventArgs e)
         {
             var newForm = new Departments();
             newForm.ShowDialog();
+        }
+
+        private void ListEmployees_Table_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Report_Menu_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            File.WriteAllText(saveFile.FileName, controller.GetSortedEmployees());
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void UpdateTable()
+        { 
+            
         }
     }
 }
